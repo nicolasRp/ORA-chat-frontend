68c855685aa3fc9c4c4ef95f7fc094bd9115c07b
Kamilo Vasquez <kamilo@softdev.team>
2017-10-18 12:10:51 -0500

    Chat styles

--- src/routes/Chat/components/Chat.jsx
+++ src/routes/Chat/components/Chat.jsx
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import MessageComposer from './MessageComposer';
 import MessageList from './MessageList';
-import {Col, Grid, Input, Row} from 'react-bootstrap';
 
 
 class Chat extends Component {
@@ -11,20 +9,9 @@ class Chat extends Component {
 
     render() {
         return (
-            <Grid>
-                <Row>
-                    
-                    <Col
-                        xs={12}
-                        sm={10} smoffset={1}
-                        md={8} mdOffset={2}
-                    >
-                        <MessageList messages={this.props.messages}/>
-                        <MessageComposer user={this.props.user} 
-                        send={this.props.send} />
-                    </Col>
-                </Row>
-            </Grid>
+            <div className="log">
+                <MessageList messages={this.props.messages}/>
+            </div>
         )
     }
 
